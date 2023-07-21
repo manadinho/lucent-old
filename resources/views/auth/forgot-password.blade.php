6267549d8624f634abb04ec878713c16a2fabb88
Muhammad Imran Israr <mimranisrar6@gmail.com>
2023-07-21 21:30:07 +0500

    back button component created and added into forget-password screen

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,5 +1,8 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
+        <x-back-button></x-back-button>
+    </div>
+    <div class="mb-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
