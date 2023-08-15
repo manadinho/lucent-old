8d6d8ef25fc2a785e8e1e348ebf1f52cd89ca148
Muhammad Imran Israr <mimranisrar6@gmail.com>
2023-08-15 19:06:15 +0500

    team member invitation related issues resolved

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
@@ -19,12 +21,15 @@ class RegisteredUserController extends Controller
      * Display the registration view.
      */
     public function create(Request $req): View
-    {  
+    {
         if(auth()->user()) {
             Auth::logout();
         }
         
-        return view('auth.register',['userEmail' => $req->email,'token'=>$req->token]);
+        return view('auth.register',[
+            'userEmail' => $req->email, 
+            'token'=>$req->token
+        ]);
     }
 
     /**
@@ -34,23 +39,55 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        $invitation = DB::table('invitation')->where('token', $request->token)->first();
+
+        if (!$invitation) {
+            return abort(403);
+        }
+
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::where('email', $request->email)->first();
+        $user = $this->updateUser($invitation, $request);
 
-        $user->update([
-            'name' => $request->name,
-            'password' => Hash::make($request->password),
-        ]);
+        $this->destroyInvitationLink();
                 
         event(new Registered($user));
 
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Update a user's information based on an invitation and request data.
+     *
+     * @param \App\Models\Invitation $invitation The invitation instance.
+     * @param \Illuminate\Http\Request $request The request instance containing updated user data.
+     *
+     * @return \App\Models\User The updated user instance.
+     */
+    private function updateUser($invitation, $request)
+    {
+        $user = User::find($invitation->user_id);
+
+        $user->update([
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $user;
+    }
+
+    /**
+     * Destroy the invitation link based on the provided token.
+     *
+     * @return void
+     */
+    private function destroyInvitationLink(): void 
+    {
+        DB::table('invitation')->where('token',request()->token)->delete();
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,18 +2,17 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
         <input type="hidden" name='token' value="{{$token}}">
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email update')" />
+            <b class="text-green-600">{{ $userEmail }}</b>
+        </div>
+
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email update')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="$userEmail" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
