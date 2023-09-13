2193bdd7fcc098fa680e3cc2bce641b9f91caacb
manadinho <mimranisrar6@gmail.com>
2023-09-13 15:23:17 +0500

    added show password checkbox in reset pasword

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
+    <form method="POST" action="{{ route('password.store') }}" x-data="{ showPassword: false }">
         @csrf
 
         <!-- Password Reset Token -->
@@ -15,7 +15,7 @@
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" x-bind:type="showPassword ? 'text' : 'password'" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -24,10 +24,17 @@
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
+                                x-bind:type="showPassword ? 'text' : 'password'"
                                 name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <label for="show_password" class="inline-flex items-center">
+                <input @click="showPassword = !showPassword" id="show_password" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Show Password') }}</span>
+            </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
